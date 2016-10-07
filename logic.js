6f00b9a046a6022ace265fd1b6baea2e0f680008
Ben Ebel <bebel@allstate.com>
2016-10-07 16:09:48 -0700

    Feature (count functionality) count functionality working
    Signed-off-by: Taylor Smith <tsmjk@allstate.com>

--- logic.js
+++ logic.js
@@ -10,6 +10,7 @@ window.onload = () => {
   for (var i = 0; i < allLinks.length; i++) {
     allLinks[i].addEventListener('click', (e) => {
       clickedContent(e.target.href)
+      getCount()
     }, true);
   }
 }
@@ -39,4 +40,15 @@ function clickedContent(url) {
 
 window.onunload = () => {
   localStorage.setItem('clickedLinks', JSON.stringify(clickedLinks))
+}
+
+function getCount() {
+  clickedLinks.forEach(function(val,index) {
+    if (val.count >= 2) {
+      console.log("a[href='"+val.url+"']");
+      document.querySelectorAll("a[href='"+val.url+"']")
+      console.log(j);
+      j.style.color="green"
+    }
+  })
 }
